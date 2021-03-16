0f0c8d2ec92d6f39e9e2af3d5e4772e0a4a2e200
yes.man <46117906+SoulRonin@users.noreply.github.com>
2021-03-16 15:09:37 +0800

    snake

--- SnakeGame/src/modules/food.ts
+++ SnakeGame/src/modules/food.ts
@@ -2,13 +2,15 @@
  * Created by Yes.Man on 2021/3/13 4:42 下午.
  * desc: 食物
  */
+import Snake from './snake';
+
 export default class Food {
-  private x: number;
-  private y: number;
-  private food: HTMLElement;
+  x: number;
+  y: number;
+  private food= document.getElementById('food')!;
+  private snake = new Snake();
 
   constructor () {
-    this.food = document.getElementById('food');
     this.genRandomFood();
   }
 
@@ -20,6 +22,23 @@ export default class Food {
     const total = 500 / 20 - 1;
     const x = Math.round(Math.random() * total) * 20;
     const y = Math.round(Math.random() * total) * 20;
+
+    // 不能在snake区域生成食物
+    let isInSnake = false;
+
+    for (let i = 0; i < this.snake.Bodies.length; i++) {
+      const { left, top } = this.snake.Bodies[i].style;
+      if (Number.parseInt(left) === x && Number.parseInt(top) === y) {
+        isInSnake = true;
+        break;
+      }
+    }
+
+    if (isInSnake) {
+      this.genRandomFood();
+      return;
+    }
+
     this.x = x;
     this.y = y;
     this.food.style.left = x + 'px';
